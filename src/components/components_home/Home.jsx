4f846ef8f6f6541a2ad293f911834d963121b90b
felipe <juanfeliperodriguezvasquez2006@gmail.com>
2025-04-07 17:27:01 -0500

    add menu and hour

--- src/components/components_home/Home.jsx
+++ src/components/components_home/Home.jsx
@@ -1,12 +1,22 @@
+
 import Menu from "./menu.jsx";
 import Header from "./Header_home.jsx";
+import Cont_home from "./cont_home.jsx";
+
 
 export const Home = () => {
   return (
     <>
       <Header />
-
-      <Menu />
+      <div className="contenedor">
+          <aside>
+            <Menu />
+          </aside>
+          <section>
+            <Cont_home/>
+          </section>
+      </div>
+      
     </>
   );
 };
--- src/components/components_home/menu.jsx
+++ src/components/components_home/menu.jsx
@@ -4,15 +4,28 @@ import Check from '../../assets/icons/check.svg'
 import Cal from '../../assets/icons/calendar.svg'
 import Contra from '../../assets/icons/sign.svg'
 import Emp from '../../assets/icons/employe.svg'
+import '../../styles/styles_home/Menu.css'
 export const Menu = () => {
   return (
-    <ul>
-        <li><img src={Barras} alt="Barras_menu" />MENU</li>
-        <li><img src={Casa} alt="Casa" />INICIO</li>
-        <li><img src={Check} alt="Check" />PERMISOS</li>
-        <li><img src={Cal} alt="Calendario" />ASISTENCIA</li>
-        <li><img src={Contra} alt="Hoja" />CONTRATO</li>
-        <li><img src={Emp} alt="Empleado" />EMPLEADOS</li>
+    <ul className='menu'>
+        <li className='titulo_menu'><img src={Barras} alt="Barras_menu" />
+          MENU
+        </li>
+        <li className='text_menu'><img src={Casa} alt="Casa" /> 
+          INICIO
+        </li>
+        <li className='text_menu'><img src={Check} alt="Check" />
+          PERMISOS
+        </li>
+        <li className='text_menu'><img src={Cal} alt="Calendario" /> 
+          ASISTENCIA
+        </li>
+        <li className='text_menu'><img src={Contra} alt="Hoja" /> 
+          CONTRATO
+        </li>
+        <li className='text_menu'><img src={Emp} alt="Empleado" /> 
+          EMPLEADOS
+        </li>
     </ul>
   )
 }
