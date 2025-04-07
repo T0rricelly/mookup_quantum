8277df778bd64ce21ef7e87a43a30c6ded3a8521
robinpuentes <robinpuentess@gmail.com>
2025-04-07 17:37:07 -0500

    table contrat

---
+++ src/components/components_home/table_employe.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components_auth/Header_Auth";
+import Footer from "../components_auth/footer";
+
+const Table_Employe = () => {
+  return (
+    <>
+      <Header />
+      <h3>CONTRATO</h3>
+      <h1>Ver contrato</h1>
+      <nav className="container_contrat"></nav>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Table_Employe;
