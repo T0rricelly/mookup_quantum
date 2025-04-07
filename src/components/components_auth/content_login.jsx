24ec7e7596b48d2b3aa932ceacf7fbb954e9a523
Santiago <kevintorres1609@gmail.com>
2025-04-07 17:43:13 -0500

    hola

--- src/components/components_auth/content_login.jsx
+++ src/components/components_auth/content_login.jsx
@@ -5,45 +5,89 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const Content_Login = () => {
-  const showToast = () => {
-    toast.error('Por favor llena todos los campos sugeridos', {
-      position: "top-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: false,
-      progress: undefined,
-      theme: "light",
-    });
-  };
-  return (
-    <>
-      <Header />
-      <main className='container'>
-        <h2 className='container__title'>Login</h2>
-        <form action="POST" className='container__form' >
-          <label htmlFor="id_card" className='container__label'>Numero de documento</label>
-          <input type="text" className='container__input' placeholder='Ingrese su cedula' name='id_card' required />
-          <label htmlFor="password" className='container__label'>Contraseña</label>
-          <input type="password" className='container__input' placeholder='Ingrese su contraseña' name='password' required />
-          <button type='submit' onClick={showToast} className='container__submit'>Ingresar</button>
-          <a href="/forgot" className='container__link'>Olvide mi contraseña</a>
-        </form>
-      </main>
-      <Footer />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />  
-    </>
-  )
+  const form = () => {
+
+    const [idCard, setidCard] = React.useState('');
+
+    const [password, setPassword] = React.useState('');
+
+    const handleIdCard = (e) => {
+      console.log(e.target.value);
+      // const idCard = document.querySelector('input[name="id_card"]').value;
+      // const regex = /^[0-9]{1,10}$/;
+      // if (!regex.test(idCard)) {
+      //   toast.error('El número de documento debe contener solo números y tener un máximo de 10 dígitos.', {
+      //     position: "top-right",
+      //     autoClose: 3000,
+      //     hideProgressBar: false,
+      //     closeOnClick: true,
+      //     pauseOnHover: false,
+      //     draggable: false,
+      //     progress: undefined,
+      //     theme: "light",
+      //   });
+    }
+    const handlePassword = (e) => {
+      console.log(e.target.value);
+    }
+    const showToast = () => {
+      toast.error('Por favor llena todos los campos sugeridos', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+    return (
+      <>
+        <Header />
+        <main className='container'>
+          <h2 className='container__title'>Login</h2>
+          <form action="" className='container__form' >
+            <label htmlFor="id_card" className='container__label'>Numero de documento</label>
+            <input
+              type="text"
+              className='container__input'
+              placeholder='Ingrese su cedula'
+              name='id_card'
+              onChange={handleIdCard}
+              required />
+            <label htmlFor="password" className='container__label'>Contraseña</label>
+            <input
+              type="password"
+              className='container__input'
+              placeholder='Ingrese su contraseña'
+              name='password'
+              onChange={handlePassword}
+              required />
+            <button
+              type='submit'
+              onClick={showToast}
+              className='container__submit'
+            >
+              Ingresar
+            </button>
+            <a href="/forgot" className='container__link'>Olvide mi contraseña</a>
+          </form>
+        </main>
+        <Footer />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </>
+    )
 }
-export default Content_Login
+
+export default Content_Login;
--- src/components/components_home/Header_home.jsx
+++ src/components/components_home/Header_home.jsx
@@ -3,15 +3,11 @@ import './../../styles/styles_home/header_home.css';
 export const Header = () => {
   return (
     <header className='header_home'>
-      {/* <div> */}
-
+      
       <img src={quantumLogo} alt="Quantum Logo" className='header__img' />
-      {/* </div> */}
-      {/* <div> */}
         <p className='rol'>
           Rol:
         </p>
-      {/* </div> */}
       <div className='buttons'>
         <button className='btn_cuenta'>Cuenta</button>
         <a className='btn_logout' href='/login'>Log out</a>
