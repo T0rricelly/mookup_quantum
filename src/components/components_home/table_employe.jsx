a8aed2c4c63a1d7902c67da0374a70a9fe9d32b8
robinpuentes <robinpuentess@gmail.com>
2025-04-08 23:36:31 -0500

    cambio en la vista de contrato

--- src/components/components_home/table_employe.jsx
+++ src/components/components_home/table_employe.jsx
@@ -1,15 +1,44 @@
 import React from "react";
-import Header from "../components_auth/Header_Auth";
+import HeaderHome from "./Header_home"; 
 import Footer from "../components_auth/footer";
+import "./../../styles/styles_home/table_employe.css";
+import Menu from "./menu";
 
 const Table_Employe = () => {
   return (
     <>
-      <Header />
-      <h3>CONTRATO</h3>
-      <h1>Ver contrato</h1>
-      <nav className="container_contrat"></nav>
+      <HeaderHome />
+      <div className="container_table_employe">
+        <Menu />
+        <div className="container_main">
+          <div className="container_contrat">
+            <h2 className="contract">CONTRATO</h2>
+            <button className="download_contrat">Descargar Contrato</button>
+          </div>
 
+          <div className="main-content">
+            <div className="grid-table">
+              <div className="grid-header">
+                <div className="grid-cell">Fecha Inicio</div>
+                <div className="grid-cell">Fecha Fin</div>
+                <div className="grid-cell">Estado</div>
+                <div className="grid-cell">Horario</div>
+                <div className="grid-cell">Tipo de Contrato</div>
+              </div>
+
+              {[...Array(7)].map((_, index) => (
+                <div className="grid-row" key={index}>
+                  <div className="grid-cell"></div>
+                  <div className="grid-cell"></div>
+                  <div className="grid-cell"></div>
+                  <div className="grid-cell"></div>
+                  <div className="grid-cell"></div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
