77eeca4573a20758fe5806278fcbb3ec92380296
Tronywhat <tomasfelipelagunaacevedo@gmail.com>
2025-04-09 23:18:15 -0500

    agregar las URL de contacto quantum

--- src/components/components_auth/footer.jsx
+++ src/components/components_auth/footer.jsx
@@ -12,17 +12,17 @@ export const Footer = () => {
             <img src={quantumLogo} alt="logo quantum" className='footer__img' />
             <div className='footer__info'>
                 <p className='footer__data'>Direccion: *direccion*</p>
-                <p className='footer__data'>Contacto: QUANTUM@gmail.com</p>
+                <p className='footer__data'>Contacto: 20quantum24@gmail.com</p>
                 <p className='footer__data'>Linea de atencion: *numero de celular*</p>
             </div>
             <div className="footer__contain">
 
                 <div className='footer__social'>
                     <h4 className='footer__title'>SIGUENOS</h4>
-                    <a href="https://www.instagram.com/quantum/" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={instagramLogo} alt="" /></a>
-                    <a href="https://www.facebook.com/quantum/" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={facebookLogo} alt="" /></a>
-                    <a href="https://www.x.com/quantum/" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={xLogo} alt="" /></a>
-                    <a href="https://www.youtube.com/quantum/" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={ytLogo} alt="" /></a>
+                    <a href="https://www.instagram.com/qua.ntum2024/" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={instagramLogo} alt="" /></a>
+                    <a href="https://www.facebook.com/profile.php?id=61575232014860&sk=about_details" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={facebookLogo} alt="" /></a>
+                    <a href="https://x.com/20Quantum24" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={xLogo} alt="" /></a>
+                    <a href="https://youtube.com/@20quantum24?si=55tWanLKQN-kKC6w" target="_blank" rel="noopener noreferrer" className='footer__link'><img src={ytLogo} alt="" /></a>
                 </div>
                 <a href="#" className='footer__politics'>Terminos y condiciones</a>
                 <a href="#" className='footer__politics'>Politica de privacidad</a>
